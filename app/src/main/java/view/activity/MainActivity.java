05ceb3a95128dac3177f4f529cd11df08d2403a1
Elena Abarova <lena.abarova@gmail.com>
2019-02-19 23:50:56 +0200

    - main page xml layout - 24h item xml layout - REST API research via Insomnia: all requests, no librray available need network core from scratch - design images for main page - dimensions, strings, colors, drawables used for xml creation

--- app/src/main/java/view/activity/MainActivity.java
+++ app/src/main/java/view/activity/MainActivity.java
@@ -3,12 +3,19 @@ package view.activity;
 import android.os.Bundle;
 
 import com.SupremeManufacture.weather.R;
+import com.SupremeManufacture.weather.databinding.ActivityMainBinding;
+
+import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends BaseActivity {
+
+    private ActivityMainBinding mActivityBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
     }
 }
